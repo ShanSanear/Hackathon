6750c2e6d9d3a51f763c65c3c9d37a96798ef75d
PruskiM <mateusz.pruski@trw.com>
2019-10-25 22:01:04 +0200

    Added simple stream solution and getting data from them. Restored time.sleep.

--- app.py
+++ app.py
@@ -1,41 +1,63 @@
 import audioop
 import json
+import time
 from datetime import datetime
 from statistics import mean
 import numpy
+import pyaudio
 
 from flask import Flask, Response, render_template
 
-from audio import stream, RATE, CHUNK, RECORD_SECONDS
-
-application = Flask(__name__)
+from audio import RATE, CHUNK, RECORD_SECONDS, FORMAT, CHANNELS
 
 
-def read():
-    frames = []
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        rms = audioop.rms(data, 2)
+class DataCollector:
+    def __init__(self):
+        self.streams = []
+        self.audios = []
 
-        frames.append(numpy.log10(rms) * 20)
-    return mean(frames)
+    def create_data_point(self):
+        # start Recording
+        audio = pyaudio.PyAudio()
+        self.audios.append(audio)
+        stream = audio.open(format=FORMAT, channels=CHANNELS,
+                            rate=RATE, input=True,
+                            frames_per_buffer=CHUNK)
+        self.streams.append(stream)
+
+    def read_from_stream(self, stream_number=0):
+        frames = []
+        stream = self.streams[stream_number]
+        stream.start_stream()
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = self.streams[stream_number].read(CHUNK)
+            rms = audioop.rms(data, 2)
+
+            frames.append(numpy.log10(rms) * 20)
+        stream.stop_stream()
+        return mean(frames)
 
 
-@application.route('/')
+app = Flask(__name__)
+data_collector = DataCollector()
+data_collector.create_data_point()
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
 
-@application.route('/chart-data')
+@app.route('/chart-data')
 def chart_data():
     def get_data():
         while True:
             json_data = json.dumps(
-                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': read()})
+                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': data_collector.read_from_stream()})
             yield f"data:{json_data}\n\n"
+            time.sleep(0.5)
 
     return Response(get_data(), mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
-    application.run(debug=True, threaded=True)
+    app.run(debug=True, threaded=True)
