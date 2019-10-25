368b89c1fb20f1e3358851df93585ed11af33d71
Zdziech Arkadiusz <arkadiusz.zdziech@trw.com>
2019-10-26 01:36:00 +0200

    Initial Database project

---
+++ HackathonBase/HackathonBase/Controllers/EntryController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using HackathonBase.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackathonBase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntryController : Controller
+    {
+        private readonly EntryContext _context;
+
+        public EntryController(EntryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Entry entry)
+        {
+            if (entry == null)
+                return NoContent();
+
+            _context.Add(entry);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Create(IEnumerable<Entry> entries)
+        {
+            if (entries == null)
+                return NoContent();
+
+            if (!entries.Any())
+            {
+                return Ok();
+            }
+
+            foreach (Entry entry in entries)
+            {
+                _context.Add(entry);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public List<Entry> Get(DateTime startTime, DateTime endTime)
+        {
+            List<Entry> entries = _context.Entries.Where(
+                x =>
+                    DateTime.Compare(x.StartTime, startTime) >= 0 &&
+                    DateTime.Compare(x.EndTime, endTime) <= 0).ToList();
+
+            return entries;
+        }
+    }
+}
---
+++ HackathonBase/HackathonBase/Models/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace HackathonBase.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //TODO : Add Device ID from Audio lib
+
+    }
+}
