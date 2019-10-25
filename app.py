88657da237c80eb8e00063790f27ea6a474cf3d4
PruskiM <mateusz.pruski@trw.com>
2019-10-25 23:31:38 +0200

    Added data collector.

--- app.py
+++ app.py
@@ -1,75 +1,21 @@
-import audioop
-import json
-import time
-from datetime import datetime
-from statistics import mean
-import numpy
-import pyaudio
+from flask import Flask, render_template, request
+from flask_apscheduler import APScheduler
 
-from flask import Flask, Response, render_template, request
+import atexit
 
-from audio import RATE, CHUNK, RECORD_SECONDS, FORMAT, CHANNELS
-
-
-class DataCollector:
-    def __init__(self):
-        self.streams = []
-        self.audios = []
-        self.create_data_point()
-        self.time_interval = 0.5
-
-    def create_data_point(self):
-        # start Recording
-        audio = pyaudio.PyAudio()
-        self.audios.append(audio)
-        stream = audio.open(format=FORMAT, channels=CHANNELS,
-                            rate=RATE, input=True,
-                            frames_per_buffer=CHUNK)
-        self.streams.append(stream)
-
-    def read_from_stream(self, stream_number=0):
-        frames = []
-        stream = self.streams[stream_number]
-        stream.start_stream()
-        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-            data = self.streams[stream_number].read(CHUNK)
-            rms = audioop.rms(data, 2)
-
-            frames.append(numpy.log10(rms) * 20)
-        stream.stop_stream()
-        return mean(frames)
-
-    def get_chart_data(self):
-        def get_data():
-            while True:
-                json_data = json.dumps(
-                    {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                     'value': data_collector.read_from_stream()}
-                )
-                yield f"data:{json_data}\n\n"
-                time.sleep(self.time_interval)
-
-        return Response(get_data(), mimetype='text/event-stream')
-
-    def get_single_stream_data(self, stream_number):
-        def get_data():
-            while True:
-                json_data = json.dumps(
-                    {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                     'value': data_collector.read_from_stream(stream_number)}
-                )
-                yield f"data:{json_data}\n\n"
-                time.sleep(self.time_interval)
-
-        return Response(get_data(), mimetype='text/event-stream')
-
-    def change_configuration(self, configuration):
-        if 'time_interval' in configuration:
-            time_interval = configuration['time_interval']
-            self.time_interval = time_interval
-
+from data_collector import DataCollector
 
 app = Flask(__name__)
+cron = APScheduler()
+cron.init_app(app)
+cron.start()
+
+
+@cron.task('interval', seconds=10)
+def job_function():
+    print("CRON JOB")
+
+
 data_collector = DataCollector()
 
 
@@ -95,4 +41,5 @@ def change_configuration():
 
 
 if __name__ == '__main__':
+    atexit.register(lambda: cron.shutdown(wait=False))
     app.run(debug=True, threaded=True)
---
+++ data_collector.py
@@ -0,0 +1,71 @@
+import audioop
+import json
+import time
+from datetime import datetime
+from statistics import mean
+
+import numpy
+import pyaudio
+from flask import Response
+
+from app import data_collector
+from audio import FORMAT, CHANNELS, RATE, CHUNK, RECORD_SECONDS
+
+
+class DataCollector:
+    def __init__(self):
+        self.streams = []
+        self.audios = []
+        self.create_data_point()
+        self.time_interval = 0.5
+
+    def create_data_point(self):
+        # start Recording
+
+        audio = pyaudio.PyAudio()
+        self.audios.append(audio)
+        stream = audio.open(format=FORMAT, channels=CHANNELS,
+                            rate=RATE, input=True,
+                            frames_per_buffer=CHUNK)
+        self.streams.append(stream)
+
+    def read_from_stream(self, stream_number=0):
+        frames = []
+        stream = self.streams[stream_number]
+        stream.start_stream()
+        for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            data = self.streams[stream_number].read(CHUNK)
+            rms = audioop.rms(data, 2)
+
+            frames.append(numpy.log10(rms) * 20)
+        stream.stop_stream()
+        return mean(frames)
+
+    def get_chart_data(self):
+        def get_data():
+            while True:
+                json_data = json.dumps(
+                    {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                     'value': data_collector.read_from_stream()}
+                )
+                yield f"data:{json_data}\n\n"
+                time.sleep(self.time_interval)
+
+        return Response(get_data(), mimetype='text/event-stream')
+
+    def get_single_stream_data(self, stream_number):
+        def get_data():
+            while True:
+                json_data = json.dumps(
+                    {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                     'value': data_collector.read_from_stream(stream_number)}
+                )
+                yield f"data:{json_data}\n\n"
+                time.sleep(self.time_interval)
+
+        return Response(get_data(), mimetype='text/event-stream')
+
+    def change_configuration(self, configuration):
+        if 'time_interval' in configuration:
+            time_interval = configuration['time_interval']
+            self.time_interval = time_interval
